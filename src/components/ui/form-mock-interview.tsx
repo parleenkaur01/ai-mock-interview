1217ec0d65342394324c7b6502025fa7a28c565b
Parleen Bagga <parleenkaurbagga@gmail.com>
2026-03-15 21:51:08 -0700

    creating the form

--- src/components/ui/form-mock-interview.tsx
+++ src/components/ui/form-mock-interview.tsx
@@ -1,19 +1,89 @@
-import {z} from "zod"
+import {set, z} from "zod"
 import {zodResolver} from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 
 import type { Interview } from "@/types"
 import { CustomBreadCrumb } from "./custom-bread-crumb"
-
+import {useEffect ,useState} from "react"
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "@clerk/clerk-react";
+import { describe } from "zod/v4/core"
+import { toast } from "sonner";
 
 interface FormMockInterviewProps {
     initialData : Interview | null
 }
+
+const formSchema = z.object({
+    position: z
+    .string()
+    .min(1, "Position is required")
+    .max(100, "Position must be 100 characters or less"),
+  description: z.string().min(10, "Description is required"),
+  experience: z.coerce
+    .number()
+    .min(0, "Experience cannot be empty or negative"),
+  techStack: z.string().min(1, "Tech stack must be at least a character"),
+
+});
+
+type FormData = z.infer<typeof formSchema>
 export const FormMockInterview = ({initialData}:FormMockInterviewProps) => {
+
+    const form = useForm<FormData>({
+        resolver: zodResolver(formSchema),
+        defaultValues: initialData || {}
+    })
+
+
+    const {isValid, isSubmitted} = form.formState;
+    const {loading,setLoading} = useState(false);
+    const navigate = useNavigate();
+    const {userId} = useAuth();
+
+    const title = initialData
+    ? initialData.position
+    : "Create a new mock interview";
+
+    const breadCrumpPage = initialData ? initialData?.position : "Create";
+    const actions = initialData ? "Save Changes" : "Create";
+    const toastMessage = initialData
+    ? { title: "Updated..!", description: "Changes saved successfully..." }
+    : { title: "Created..!", description: "New Mock Interview created..." };
+
+    const onSubmit = async(data:FormData)=>{
+        try{
+            setLoading(true);
+
+        }catch(error){
+            console.log(error);
+            toast.error("Error..",{
+                description: `Something went wrong.Please try again later`,
+            });
+        }finally{
+            setLoading(false);
+        }
+
+    }
+
+    useEffect(()=>{
+        if(initialData){
+            form.reset({
+
+                position: initialData.position,
+                description: initialData.description,
+                experience: initialData.experience,
+                techStack: initialData.techStack
+            })
+            
+
+
+        }
+    },[initialData,form])
     return(
-        <div className="w-full flex-col spcae-y-4">
+        <div className="w-full flex-col space-y-4">
         <CustomBreadCrumb
-            breadCrumbPage={breadCrumbPage}
+            breadCrumbPage={breadCrumpPage}
             breadCrumpItems={[{label:"Mock Interviews", link:"/generate"}]}
              />
 
